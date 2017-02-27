bbdd5670ae9c269c4cf004c36bd4f9b08af2f202
Samuel David De La Cruz <alphaelena@gmail.com>
2017-02-26 22:56:02 -0400

    -disabled classical websocket emits

--- ResourceMonitor.js
+++ ResourceMonitor.js
@@ -13,112 +13,111 @@ var _interval = 1;
 var TOTAL_PERCENT = 100;
 (function (ResourceMonitor) {
 
-ResourceMonitor.init=function(interval){
+    ResourceMonitor.init = function (interval) {
 
-app.get('/cpucount', function(req, res){
-    var cpuCount = { count:os.cpus().length }
-    res.send(cpuCount);
-});
-app.get('/availableDisks', function(req, res){
-
-    getDisks(function(error,disks){
-        currentdisks = disks;
-        res.send(disks);
-    }); 
-
-});
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-    http.listen(port, function () {
-       
-
-    StartEmitting(interval);   
-    initclassicWs();
-    io.on('connection',function(socket){
-
-    socket.on('disconnect',function(){
-        //console.log('socket %s has disconnected' .socket.id);
-     
+        app.get('/cpucount', function (req, res) {
+            var cpuCount = {count: os.cpus().length}
+            res.send(cpuCount);
         });
-    }); 
+        app.get('/availableDisks', function (req, res) {
 
-              
-});
+            getDisks(function (error, disks) {
+                currentdisks = disks;
+                res.send(disks);
+            });
+
+        });
+
+        app.get('/', function (req, res) {
+            res.sendFile(__dirname + '/index.html');
+        });
+
+        http.listen(port, function () {
 
 
+            StartEmitting(interval);
+            //initclassicWs();
+            io.on('connection', function (socket) {
 
-}
+                socket.on('disconnect', function () {
+                    //console.log('socket %s has disconnected' .socket.id);
 
-function initclassicWs(){
-var server = require('http').createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
-    server.listen(5000, function () {
-        console.log((new Date()) + ' Classic websocket on port ' + 5000);
-});
+                });
+            });
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production 
-    // applications, as it defeats all standard cross-origin protection 
-    // facilities built into the protocol and the browser.  You should 
-    // *always* verify the connection's origin and decide whether or not 
-    // to accept it. 
-    autoAcceptConnections: false
-});
+        });
 
-wsServer.on('request',function(request){
-console.log(request);
-    var connection = request.accept('echo-protocol', request.origin);
-    connections.push(connection);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
+    };
 
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-        //implement removal of connection here
-    });
+    function initclassicWs() {
 
-    
-})
-}
-function StartEmitting(interval){
-    console.log('one single interval run, start emitting signals!');
+        var server = require('http').createServer(function (request, response) {
+            console.log((new Date()) + ' Received request for ' + request.url);
+            response.writeHead(404);
+            response.end();
+        });
+        server.listen(5500, function () {
+            console.log((new Date()) + ' Classic websocket on port ' + 5000);
+        });
+
+        wsServer = new WebSocketServer({
+            httpServer: server,
+            // You should not use autoAcceptConnections for production
+            // applications, as it defeats all standard cross-origin protection
+            // facilities built into the protocol and the browser.  You should
+            // *always* verify the connection's origin and decide whether or not
+            // to accept it.
+            autoAcceptConnections: false
+        });
+
+        wsServer.on('request', function (request) {
+            console.log(request);
+            var connection = request.accept('echo-protocol', request.origin);
+            connections.push(connection);
+            console.log((new Date()) + ' Connection accepted.');
+            connection.on('message', function (message) {
+                if (message.type === 'utf8') {
+                    console.log('Received Message: ' + message.utf8Data);
+                    connection.sendUTF(message.utf8Data);
+                }
+                else if (message.type === 'binary') {
+                    console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+                    connection.sendBytes(message.binaryData);
+                }
+            });
+
+            connection.on('close', function (reasonCode, description) {
+                console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+                //implement removal of connection here
+            });
+
+
+        })
+    }
+
+    function StartEmitting(interval) {
+        console.log('one single interval run, start emitting signals!');
         if (interval) {
             _interval = interval;
         }
-  setInterval(function () {
+        setInterval(function () {
 
-        emitCPUInfoThroughSocket();
-        emitRaminfoThroughSocket();
-        emitDiskSpace();
+            emitCPUInfoThroughSocket();
+            emitRaminfoThroughSocket();
+            emitDiskSpace();
 
-    }, (_interval * 1000));
-}
+        }, (_interval * 1000));
+    }
 
 
 
-    
+
     function calculateCPUsTimeDiffs() {
         var cpus = os.cpus();
         var CPUsTimeDiffs = [];
 
         cpus.forEach(function (cpu, cpuindex) {
-           // console.log("CPU %s:", cpuindex);
+            // console.log("CPU %s:", cpuindex);
             var oldcpu = oldcpus[cpuindex];
 
             Object.keys(cpu.times).forEach(function (CpuTimetype) {
@@ -156,48 +155,47 @@ function StartEmitting(interval){
             io.emit(cpuData.cpu, cpuData)
         });
 
-         connections.forEach(function(connection){
-             // console.log(connection);
-                  
-                    if(connection){
-                        connection.sendUTF(JSON.stringify({CPUDATA:cpusArray}));
-                    }
-            });
+        connections.forEach(function (connection) {
+            // console.log(connection);
+
+            if (connection) {
+                connection.sendUTF(JSON.stringify({CPUDATA: cpusArray}));
+            }
+        });
         oldcpus = os.cpus();
     }
     function emitRaminfoThroughSocket(){
-      io.emit('RAMusagePercent', calculateUsedMemoryPercent())
-      connections.forEach(function(connection){
+        io.emit('RAMusagePercent', calculateUsedMemoryPercent())
+        connections.forEach(function (connection) {
             if(connection){
-             connection.sendUTF("RAMusagePercent:"+calculateUsedMemoryPercent());
+                connection.sendUTF("RAMusagePercent:" + calculateUsedMemoryPercent());
             }
-      });
+        });
     }
     function calculateUsedMemoryPercent(){
-   
+
         var freeMemoryPercent = (( os.freemem()  / os.totalmem())*100)
         var usedMemorypercent =  TOTAL_PERCENT - freeMemoryPercent
-           // console.log("memory used percent %d %:",usedMemorypercent)
-   return usedMemorypercent;
+        // console.log("memory used percent %d %:",usedMemorypercent)
+        return usedMemorypercent;
     }
 
     function emitDiskSpace(){
 
         currentdisks.forEach(function(disk){
-        var mountPath = disk.mountpoint.split(',')[0];
-        
-        diskspace.check(mountPath, function (err, total, free, status)
-          {
-              if(err){
-                  console.log(err)
-              }
-              var freeDiskPercent = (free / total)*100
-              var usedDiskSpacePercent = TOTAL_PERCENT - freeDiskPercent;
-              io.emit(mountPath,usedDiskSpacePercent);
-         
-             //console.log('used space for drive %s %d %',mountPath,usedDiskSpacePercent);                          
-          });
-           
+            var mountPath = disk.mountpoint.split(',')[0];
+
+            diskspace.check(mountPath, function (err, total, free, status) {
+                if (err) {
+                    console.log(err)
+                }
+                var freeDiskPercent = (free / total) * 100
+                var usedDiskSpacePercent = TOTAL_PERCENT - freeDiskPercent;
+                io.emit(mountPath, usedDiskSpacePercent);
+
+                //console.log('used space for drive %s %d %',mountPath,usedDiskSpacePercent);
+            });
+
         });
     }
     function getDisks(ongetDisks){
